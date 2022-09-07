a352e5cc4aa3573f7d6002013b54c4b39d41a19a
mosnjadoon <mohsinjadoon@gmail.com>
2022-09-07 23:51:26 +0500

    first demo project for elec bill cals

---
+++ elecbillcalc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//#include<conio.h>
+#include <stdlib.h>
+
+/* void clrscr()
+{
+    system("@cls||clear");
+} */
+void main(void)
+{
+ int unit;
+char a;
+ 
+printf("Enter Number of Units : \n");
+scanf("%d", &unit);
+
+printf("\nunits consumed are:  %d \n ", unit); 
+
+system("cls");
+
+printf("######################: \n");
+getch(a);
+}
